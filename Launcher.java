eb6471c0736c4b1c86a8a63a130da24fb53471e0
laristein007 <89949288+laristein007@users.noreply.github.com>
2026-06-10 07:58:32 -0400

    Create Launcher class for app execution
    Added a Launcher class to facilitate app execution.

---
+++ Launcher.java
@@ -0,0 +1,12 @@
+//Launcher
+//Dummy class to run the app, helps with facilitating exportation
+
+package com.example.focusflow;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        FocusFlowApp.main(args);
+    }
+}
