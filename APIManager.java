2d2d8704f1d547076d8405856ccf05596ab78b97
robertdayton92 <ahnrdayton2018@gmail.com>
2026-06-20 09:02:37 -1000

    Delete APIManager.java

--- APIManager.java
+++
@@ -1,9 +0,0 @@
-package focusflow;
-
-public class APIManager {
-    public String fetchInternetRadioStatus() {
-        // Placeholder for future API connection.
-        // This can later be used for music player or internet radio features.
-        return "API feature not connected yet.";
-    }
-}
